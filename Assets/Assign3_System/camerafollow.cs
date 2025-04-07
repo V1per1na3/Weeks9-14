752ed13a4176b5e58ae35a62eb254d089ee009d2
V1per1na3 <kayval135@gmail.com>
2025-04-07 06:42:51 -0400

    create a player that can move with wasd or arrow keys and is constrain to the map range. create a virtual camera that "follows" the player, and "stops follow" when player exceed certain range so that player can touches the edge of map

---
+++ Assets/Assign3_System/camerafollow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour
+{
+    //this script is to create a "fake" transform for the camera to follow.
+    //so player can touch the edge of map and it will not be always in the center of camera when its about to hit map boundary
+
+    public Transform realplayer;// this is the tranform of actual player
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 followpos = transform.position;
+        //only update this follow item's transform.x when player.x is within certain range
+        if(realplayer.transform.position.x > -8f && realplayer.transform.position.x < 8f)
+        {
+            followpos.x = realplayer.position.x;
+        }
+        //only update this follow item's transform.y when player.y is within certain range
+        if (realplayer.transform.position.y > -8f && realplayer.transform.position.y < 8f)
+        {
+            followpos.y = realplayer.position.y;
+        }
+        transform.position = followpos;
+    }
+}
---
+++ Assets/Assign3_System/playerCTRL.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCTRL : MonoBehaviour
+{
+    //this script is for the player control//
+    //player moves with WASD or arrowkeys and constrain within a fixed size of map//
+    SpriteRenderer sr;
+    float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+        checkedge();
+    }
+
+    void movement()
+    {
+        //keyboard ctrl of player
+        //get horizontal input
+        float direction = Input.GetAxis("Horizontal");
+        //get vertical input
+        float direction2 = Input.GetAxis("Vertical");
+        //flip sprite base on dir
+        sr.flipX = direction < 0;
+        //move player base on input with fixed speed
+        transform.position += transform.right * direction * speed * Time.deltaTime;
+        transform.position += transform.up * direction2 * speed * Time.deltaTime;
+    }
+
+    void checkedge()
+    {
+        //constrain player within the map
+        Vector2 pos = transform.position;
+        Vector2 screenpos = Camera.main.WorldToScreenPoint(pos);
+        //check if player leaves screen left
+        if (screenpos.x <= 0)
+        {
+            pos.x = Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x;//lock player x 
+        }
+        //check if player leaves screen right
+        if (screenpos.x >= Screen.width)
+        {
+            pos.x = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x;//lock player x
+        }
+        //check if player leaves screen top
+        if (screenpos.y >= Screen.height)
+        {
+            pos.y = Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y;//lock player y
+        }
+        //check if player leaves screen bottom
+        if (screenpos.y <= 0)
+        {
+            pos.y = Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y;//lock player y
+
+        }
+
+        transform.position = pos;
+
+    }
+}
